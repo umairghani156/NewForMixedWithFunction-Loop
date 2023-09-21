91669be41a704e391aa8f182ca568769fe7f4429
umair <umairghani156@gamil.com>
2023-09-21 15:57:34 +0500

    Function with callback of another function

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ for (var i = 0; i < studentsName.length; i++) {
 }
 console.log(studentsName);
 console.log(emptyName);
-*/
+
 let gas = [50, 90, 100];
 let food = [10, 40, 60];
  function calculateTotal(arr) {
@@ -67,3 +67,103 @@ let food = [10, 40, 60];
    food: foodTotal,
    random: ramdomTotal,
  })
+ */
+// var x = window.prompt("What is your name?", "Umair");
+// var correctAnswer = "Vetican";
+// if (x === correctAnswer) {
+// alert("Correct!");
+// }
+// var num = 7;
+// let num1 = 8;
+// const total = num >= num1;
+// total ? console.log(`${num} is greater than ${num1}`): total?console.log("it is same"): console.log("It's not same");
+/*const globalNumber = 5;
+function add(num1,num2) {
+    const result = num1 + num2 + globalNumber;
+    return result;
+}
+console.log(add(3,4));*/
+/*const num = 100;
+function myCalculate(para1,para2) {
+    let num1 = 15;
+    const result = (para1 + para2) / num1 ;
+    return result;
+}
+console.log(myCalculate(50, 50));*/
+
+/*const num = 2;
+function myCalculate(para1, para2) {
+    const num = 7;
+    const result = para1 + para2 + num;
+    return result;
+}
+console.log(myCalculate(10, 10));*/
+
+// const globalNumber = 12;
+
+// function myCalculate(para1, para2) {
+//     const globalNumber = 15;
+//     const result = para1 + para2 + globalNumber;
+//     function add() {
+//      const addResult = result * globalNumber;
+//      console.log(addResult);
+//     }
+//     add();
+//     return result;
+// }
+// console.log(myCalculate(4, 3));
+
+// const globalNumber = 10;
+// function myCalculate(num1, num2) {
+//     const globalNumber = 20;
+//     const result = num1 + num2 + globalNumber;
+//     function multiply(){
+//      const globalNumber = 100;
+//      const multiplyResult = result * globalNumber;
+//      console.log(multiplyResult);
+//     }
+//     multiply();
+//     return result;
+// }
+// console.log(myCalculate(4, 3));
+const globalNumber = 10;
+function myCalculate(num1, num2) {
+    const globalNumber = 20;
+    const result = num1 + num2 + globalNumber;
+    function multiply(num3, num4){
+     const globalNumber = 100;
+     const multiplyResult = (num3 + num4) * globalNumber;
+     console.log(multiplyResult);
+    }
+    multiply(4, 3);
+    return result;
+}
+console.log(myCalculate(4, 3));
+const myNumber = 20;
+function myComment(myNumber) {
+    return `Welcome to the world ${myNumber.toUpperCase()}`;
+}
+function myFriend(myNumber, cb) {
+ const myName = "Umair";
+ console.log(`${cb(myNumber)}, my name is ${myName}`);
+}
+myFriend('Asim',myComment);
+myFriend(`habib`,myComment);
+
+function num(num1) {
+    return `${num1} is greater than ${50}`;
+}
+function next(num1, call){
+    const totalNumber = 100;
+    console.log(`${call(num1)} is less than ${totalNumber}`);
+}
+next(70, num);
+
+function friendName(name1 , name2) {
+      return `${name1.toUpperCase()} and ${name2.toUpperCase()} are my best friends`
+}
+function myCall(name1, name2, cb) {
+    const myName = "Umair";
+    console.log(`${cb(name1,name2)} and my name is ${myName}`);
+}
+myCall("Habib","Asim",friendName);
